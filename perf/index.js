03e2aa1fd0d9c2bc14f78af347ff9dc6d1cccc08
iamdevonbutler <jpescione@gmail.com>
2017-07-04 09:02:23 -0400

    refactoring

--- perf/index.js
+++ perf/index.js
@@ -4,7 +4,7 @@ const microtime = require('microtime');
 const jsonf = require('../lib');
 
 const st = microtime.now();
-var n = 100000;
+var n = 1000000;
 var i = n;
 var str = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nobis dignissimos, quas! Est asperiores ab beatae ex, dolor optio nostrum rerum aspernatur in corporis unde aperiam error fuga ut, illo?';
 
@@ -14,14 +14,23 @@ var obj = {
     c: 'string',
     d: [1,2,3],
   },
-  e: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, labore adipisci accusantium! Iure, corporis quia numquam ea error nam sunt. Labore eveniet, nobis eos tempore quis non numquam incidunt nisi!'
+  e: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, labore adipisci accusantium! Iure, corporis quia numquam ea error nam sunt. Labore eveniet, nobis eos tempore quis non numquam incidunt nisi!',
+  f: {},
+  g: [],
 };
 
+var x = 1;
 while (i--) {
+  // var x = [1].concat([2]);
+  // var x = [1].unshift(2);
   // JSON.stringify(obj);
-  var x = jsonf.stringify(obj);
-  console.log(x);
+  var str = jsonf.stringify(obj);
+  console.log(str);
+  var obj = jsonf.parse(str);
+  console.log(obj);
   return;
+  // var x = {a: 1, b: 2};
+  // var a = x.a;
 }
 
 const ms = (microtime.now() - st) / 1000;
