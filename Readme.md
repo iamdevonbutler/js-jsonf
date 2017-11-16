977a13381b476cd377572d017a0d95ed35c5231e
iamdevonbutler <jpescione@gmail.com>
2017-11-16 10:29:09 -0500

    added doxygens

---
+++ Readme.md
@@ -0,0 +1,2 @@
+function behavior is normalized and consistent; however, the parsed function may contain formatting differences from its original.
+it supports date obj too.
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,167 +1,78 @@
+'use strict';
+
+// no async generators
+
 const should = require('chai').should();
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 
-const jsonf = require('../lib');
+const {stringify, parse} = require('../lib');
 
 var obj = {
-  // b: '$a Lorem ipsum dolor sit amet, consec,tetur adipisicing elit. Voluptatem quis earum pariatur iste, repudiandae, quo laboriosam. Eos odit, tempore reprehenderit eius minima soluta corporis ducimus, totam quam obcaecati ex ad.',
-  b: '$a Lorem ipsum dolor sit amet, consec,tetur adipisicing elit. Voluptatem quis earum pariatur iste, repudiandae, quo laboriosam. Eos odit, tempore reprehenderit eius minima soluta corporis ducimus, totam quam obcaecati ex ad.',
-  c: false,
-  d: true,
-  e: NaN,
-  f: 1.2,
-  g: [],
-  h: null,
-  i: undefined,
-  j: {},
-  k: new Date(),
-  l: function() {
-    // return 1;
+  a: false,
+  b: true,
+  c: NaN,
+  d: null,
+  e: undefined,
+  f: '',
+  g: 'a$a () "" | ,,\'\'Lorfunction() {return 1}, () => {},eval("throw new Error(111)") em ipsum dolor sit amet, consec,tetur adipisicing elit. Voluptatem quis earum pariatur iste, repudiandae, quo laboriosam. Eos odit, tempore reprehenderit eius minima soluta corporis ducimus, totam quam obcaecati ex ad.',
+  h: 0,
+  i: 1.2,
+  j: -1.2,
+  k: [],
+  l: {},
+  m: new Date(),
+  n: function() {
+    function aaa() {}
+    return 1;
   },
-  // l() {
-  //   return 1;
-  // }
-  // $a: 1, // @todo test
-  // @todo add items to array.
+  o: () => {
+    return 1;
+  },
+  p() {
+    return 1;
+  },
+  q: function* () {
+    return 1;
+  },
+  r: function* a() {
+    return 1;
+  },
+  s: async function() {
+    return 1;
+  },
+  t: () => {
+    return 1;
+  },
+  async u() {
+    return 1;
+  },
+  v: async () => {
+    return 1;
+  },
 };
 
 var obj1 = {
   ...obj,
-  g: [obj, obj],
-  j: obj,
+  w: [obj, obj],
+  x: obj,
 };
 
-
-
 describe('jsonf', () => {
-
-  it('should deconstruct an anonmyous function (0)', () => {
-    var obj = jsonf.deconstructFunction( () => {} );
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: null,
-    });
+  it ('should work properly', () => {
+    var obj2 = stringify(obj);
+    // console.log(obj2);
+    var obj3 = parse(obj2);
+    console.log(obj3);
+    expect(obj3).to.deep.eql(obj);
   });
 
-  it('should deconstruct an anonmyous function (1)', () => {
-    var obj = jsonf.deconstructFunction( () => {console.log(1)} );
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
-
-  it('should deconstruct an anonmyous function (2)', () => {
-    var obj = jsonf.deconstructFunction( () => console.log(1) );
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
-
-  it('should deconstruct an async function (0)', () => {
-    var obj = jsonf.deconstructFunction( async () => console.log(1) );
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: true,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
-
-  it('should deconstruct an async function (1)', () => {
-    var obj = jsonf.deconstructFunction( async() => console.log(1) );
-    expect(obj).to.eql({
-      isGenerator: false,
-      isAsync: true,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
-
-  it('should deconstruct an generator function (0)', () => {
-    var obj = jsonf.deconstructFunction( function* () { console.log(1) } );
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
-
-  it('should deconstruct an generator function (1)', () => {
-    var obj = jsonf.deconstructFunction( function * () { console.log(1) } );
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
-
-  it('should deconstruct an generator function (2)', () => {
-    var obj = jsonf.deconstructFunction( function *() { console.log(1) } );
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: null,
-      params: null,
-      body: 'console.log(1)',
-    });
-  });
-
-  it('should deconstruct an generator function (3)', () => {
-    var obj = jsonf.deconstructFunction(function * aa() {function aaa() {return 2;}return aaa();});
-    expect(obj).to.eql({
-      isGenerator: true,
-      isAsync: false,
-      name: 'aa',
-      params: null,
-      body: 'function aaa() {return 2;}return aaa();',
-    });
-  });
-
-  it('should should', () => {
-    var obj = jsonf.deconstructFunction(function aa() {
-      function aaa() {
-        return 2;
-      }
-      return aaa();
-    });
-    console.log(obj);
-  });
+  // it ('should work properly', () => {
+  //   var obj2 = stringify(obj1);
+  //   // console.log(obj2);
+  //   var obj3 = parse(obj2);
+  //   console.log(obj3);
+  //   expect(obj3).to.deep.eql(obj1);
+  // });
 
 });
-
-// function type | async | generator | name | params | body
-
-// var trials = [];
-//
-// var types = [
-//   (isAsync) => {
-//     return `${isAsync ? 'async ' : ''}${}`;
-//   }
-// ];
-
-
-
-// describe(() => {
-//
-//
-//
-//
-// });
